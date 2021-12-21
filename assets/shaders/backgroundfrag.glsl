80479243f69f5427b9afa6d452f9fbfa5600f293
RattyGirl <chloeholmes8192@gmail.com>
2021-12-21 13:24:21 +1100

    set glfw as submodule

--- assets/shaders/backgroundfrag.glsl
+++ assets/shaders/backgroundfrag.glsl
@@ -7,7 +7,5 @@ out vec3 color;
 
 void main(){
     vec3 textureColor = texture( myTextureSampler, uv ).rgb;
-    if (textureColor.rgb == vec3(1.0, 0.0, 0.99607843))
-    discard;
     color = textureColor;
 }
